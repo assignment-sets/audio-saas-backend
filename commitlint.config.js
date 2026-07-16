7bac52c16fbc10e08ef221308defbccb5058c52b
assignment-sets <mondalgourab140@gmail.com>
2026-07-16 19:03:28 +0530

    chore: update commitlint config to extend commit msg header max length to 200 from 100 chars

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // 0 = disable, 2 = error. 'always' means enforce it. 200 is the new max length.
+    'header-max-length': [2, 'always', 200],
+  },
+};
