08fc2c8c5b8567f88d1ead09b0fff3de37477c19
assignment-sets <mondalgourab140@gmail.com>
2026-08-05 19:01:55 +0530

    chore: allow unrestricted body line length in commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,6 @@ export default {
   rules: {
     // 0 = disable, 2 = error. 'always' means enforce it. 200 is the new max length.
     'header-max-length': [2, 'always', 200],
+    'body-max-line-length': [0, 'always'], // allow unrestricted body line length for descriptive commits
   },
 };
